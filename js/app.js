32cfbbcf0583596810f3f7987814538f73fc1ee3
Ghislain Faure <ghislainfaure06@gmail.com>
2021-11-24 11:47:10 +0100

    etape1-etape2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-// ton code ici
+// on crée une boucle pour génerer 8 * 8 ( 32 ) cases 
+let invader = document.getElementById('invader');
+
+
+
+for (let i = 0; i < 64; i++) {
+    let newCase = document.createElement('div');
+    newCase.classList.add('case');
+
+    invader.appendChild(newCase);
+    
+
+}
+
+let pixel = document.getElementsByClassName('case');
+console.log(pixel);
+invader.addEventListener('click', function(event){
+    //console.log(event);
+   console.log(event.target); 
+   //event.target.style.backgroundColor = 'white';
+   event.target.classList.toggle('black')
+
+})
+
+
+
+ 
