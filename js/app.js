5c11742c634a35fca2565f58105b7a1f7abe3cc8
Ghislain Faure <ghislainfaure06@gmail.com>
2021-11-24 15:48:11 +0100

    étape3-terminée

--- js/app.js
+++ js/app.js
@@ -1,27 +1,52 @@
-// on crée une boucle pour génerer 8 * 8 ( 32 ) cases 
+// on crée une boucle pour génerer 8 * 8 ( 64 ) cases 
 let invader = document.getElementById('invader');
 
 
+ function generateTable(nbPixel) {
 
-for (let i = 0; i < 64; i++) {
-    let newCase = document.createElement('div');
-    newCase.classList.add('case');
+     for (let i = 0; i < nbPixel; i++) {
 
-    invader.appendChild(newCase);
+        let row = document.createElement('div')
+        invader.appendChild(row)
+
+             for (let y = 0; y < nbPixel; y++) {
+               
+                 let newCase = document.createElement('div');
+                 newCase.classList.add('case');
+                 row.appendChild(newCase);
+             }
+
+     }
+ }
+
+
+// on crée le formulaire
+let form = document.querySelector('.configuration')
+
+let sizePixel = document.createElement('input');
+sizePixel.id = "sizePixel";
+form.appendChild(sizePixel);
+
+let submit = document.createElement('button');
+submit.type = 'button';
+submit.textContent = 'Valider';
+form.appendChild(submit);
+
+
+// création de notre notre matrice Pixel
+
+submit.addEventListener('click', function() {
     
-
-}
-
-let pixel = document.getElementsByClassName('case');
-console.log(pixel);
-invader.addEventListener('click', function(event){
-    //console.log(event);
-   console.log(event.target); 
-   //event.target.style.backgroundColor = 'white';
-   event.target.classList.toggle('black')
+    invader.textContent = '';
+    generateTable(sizePixel.value);
 
 })
 
+// Quand on click sur le bouton on veut changer la class de chaque pixel ( background color )
 
+invader.addEventListener('click', function (event) {
 
- 
+    event.target.classList.toggle('black');
+
+})
+
