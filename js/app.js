ef7f78e0d0a741858cef7633eb97a91b380cdba5
GhislainFaure <ghislainfaure06@gmail.com>
2023-11-28 16:01:05 +0100

    structure projet

--- js/app.js
+++ js/app.js
@@ -1,52 +1,48 @@
-// on crée une boucle pour génerer 8 * 8 ( 64 ) cases 
-let invader = document.getElementById('invader');
+// // on crée une boucle pour génerer 8 * 8 ( 64 ) cases
+// let invader = document.getElementById('invader');
 
+//  function generateTable(nbPixel) {
 
- function generateTable(nbPixel) {
+//      for (let i = 0; i < nbPixel; i++) {
 
-     for (let i = 0; i < nbPixel; i++) {
+//         let row = document.createElement('div')
+//         invader.appendChild(row)
 
-        let row = document.createElement('div')
-        invader.appendChild(row)
+//              for (let y = 0; y < nbPixel; y++) {
 
-             for (let y = 0; y < nbPixel; y++) {
-               
-                 let newCase = document.createElement('div');
-                 newCase.classList.add('case');
-                 row.appendChild(newCase);
-             }
+//                  let newCase = document.createElement('div');
+//                  newCase.classList.add('case');
+//                  row.appendChild(newCase);
+//              }
 
-     }
- }
+//      }
+//  }
 
+// // on crée le formulaire
+// let form = document.querySelector('.configuration')
 
-// on crée le formulaire
-let form = document.querySelector('.configuration')
+// let sizePixel = document.createElement('input');
+// sizePixel.id = "sizePixel";
+// form.appendChild(sizePixel);
 
-let sizePixel = document.createElement('input');
-sizePixel.id = "sizePixel";
-form.appendChild(sizePixel);
+// let submit = document.createElement('button');
+// submit.type = 'button';
+// submit.textContent = 'Valider';
+// form.appendChild(submit);
 
-let submit = document.createElement('button');
-submit.type = 'button';
-submit.textContent = 'Valider';
-form.appendChild(submit);
+// // création de notre notre matrice Pixel
 
+// submit.addEventListener('click', function() {
 
-// création de notre notre matrice Pixel
+//     invader.textContent = '';
+//     generateTable(sizePixel.value);
 
-submit.addEventListener('click', function() {
-    
-    invader.textContent = '';
-    generateTable(sizePixel.value);
+// })
 
-})
+// // Quand on click sur le bouton on veut changer la class de chaque pixel ( background color )
 
-// Quand on click sur le bouton on veut changer la class de chaque pixel ( background color )
+// invader.addEventListener('click', function (event) {
 
-invader.addEventListener('click', function (event) {
+//     event.target.classList.toggle('black');
 
-    event.target.classList.toggle('black');
-
-})
-
+// })
